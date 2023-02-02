9a68658cc71d819b547065b74ad831b058464142
qwerty <qwert@gmail.com>
2023-02-02 19:13:10 +0500

    last

---
+++ script.js
@@ -0,0 +1,10 @@
+let audio = document.querySelector('audio')
+let btn = document.querySelector('button')
+
+btn.addEventListener('click',function () {
+  let flag = true
+  if (flag == true) {
+    audio.play()
+  }
+
+})
